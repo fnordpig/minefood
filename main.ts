08239e4383f9365f60a8a765a5e8685442c4094b
WMR <github@waughonline.com>
2022-04-21 18:18:39 -0700

    Add food attacks!

--- main.ts
+++ main.ts
@@ -1,35 +1,227 @@
 function createFoodEnemy () {
-    myEnemy = sprites.create(img`
-        . . . . . . . . . . . . . . 
-        . . . . . f f f f . . . . . 
-        . . . f f 5 5 5 5 f f . . . 
-        . . f 5 5 5 5 5 5 5 5 f . . 
-        . f 5 5 5 5 5 5 5 5 5 5 f . 
-        c b 5 5 5 d b b d 5 5 5 b c 
-        f 5 5 5 b 4 4 4 4 b 5 5 5 f 
-        f 5 5 c c 4 4 4 4 c c 5 5 f 
-        f b b f b f 4 4 f b f b b f 
-        f b b e 1 f d d f 1 e b b f 
-        c f b 4 4 d d d d d f b f c 
-        . c d d d 4 9 9 9 6 c e c . 
-        . c 4 d d 4 9 9 9 9 c 4 e . 
-        . . c e e b b b 3 b b c e . 
-        . . c c 3 b 3 b 3 3 c c . . 
-        . . . . c c c b b c . . . . 
-        `, SpriteKind.Enemy)
-    myEnemy.follow(mySprite, 10)
-    myEnemy.setPosition(randint(0, 160), randint(0, 120))
+    mySprite2 = sprites.create(foodTypes._pickRandom(), SpriteKind.Enemy)
+    mySprite2.follow(mySprite, 10)
+    mySprite2.setPosition(randint(0, 160), randint(0, 120))
 }
-controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-    if (mySprite.overlapsWith(myEnemy)) {
-        effects.hearts.startScreenEffect(500)
-        scene.cameraShake(4, 500)
-        mySprite.sayText("Tasty!!", 500, true)
-        myEnemy.destroy()
-        info.changeScoreBy(1)
-        createFoodEnemy()
-    }
-})
+function initializeGame () {
+    foodTypes = [
+    img`
+        ..............eeeeeee...........
+        ............ee455662e2e.........
+        ..........ee45556723e2688.......
+        .........e46776677232e777668....
+        ........e46745554772227776778...
+        .......4448744444777766777678...
+        ......4522e7777776777766676668..
+        .....4523227766722e666eeeee888..
+        ....455232e76672322e4555dddd48..
+        ...44567777554623e455ddddddddd4.
+        ...e66774554477e455dddd55554dd44
+        ..e46777444677e55dd55555d55dddd4
+        ..e5668677666e5dd555555555555dde
+        .e45544e8776e5d555554555555555de
+        .e554eeee66e5d5555d55555dddd54de
+        .e55ee44fee5d5d555555d5d5dddddde
+        e454eeeefe45d55555555555dd4ddde.
+        e5e4eefffe5d55555555d5555dddde..
+        e5ee4eeff45d555555555555dddde...
+        e5eeeeffe5d55d555d5555d5ddde....
+        e5ffefeee5d55545555555ddd4e.....
+        e5ffffffe545555555d5d4ddee......
+        e54efeff45d55d55555dddde........
+        e5eeeffe5dd5555545dddee.........
+        e4eeefff5d5555d55ddde...........
+        e4efefff5d5d55555d4e............
+        .e4efffe5d555555dee.............
+        .e54eeee5d545dd4e...............
+        ..e554ee5dddddee................
+        ...ee5544dddee..................
+        .....eeeeeee....................
+        ................................
+        `,
+    img`
+        ..............bbbbbbb...........
+        ...........bb66663333baa........
+        .........bb3367776333663aa......
+        ........b33333888333389633aa....
+        .......b3333333333333389633aa...
+        ......b34443333333333338633bae..
+        .....b3455433333333334443333ae..
+        ....b33322333dddd3333455233daee.
+        ...b3d333333dd3bbbb33322333dabe.
+        ..b3d333333d3bb33bb33333333da4e.
+        ..bd33333333b33aab3333333223a4ee
+        .b3d3663333b33aab33366332442b4ee
+        .bd3b983333a3aa3333387633ee3b4ee
+        .bd6983333baaa333333387633bb4bee
+        b3d6833333bba333333333863ba44ebe
+        bdd3333333bb3333333333333a44bebe
+        add666633333322333366333ba44bbbe
+        ad67776333332442336983d3a444b4e.
+        add888b333333ee3369833d3a44b44e.
+        add333333333333336833d3a444b4e..
+        a3dd3333344433333dddd3a444b44e..
+        ab33ddd325543333dd33aa444b44e...
+        .eabb3dd32233333baaa4444b44e....
+        .ebabb3d333d33baa444443b44e.....
+        ..ebaab3ddd3aaa4444433b44e......
+        ..eebbaab33a44444333b444e.......
+        ...eeebbaab444b333b4444e........
+        ....ebeeebbbbbbbb4444ee.........
+        .....eebbbb44444444ee...........
+        .......eeebbb444eee.............
+        ..........eeeeee................
+        ................................
+        `,
+    img`
+        . . . . . . . e c 7 . . . . . . 
+        . . . . e e e c 7 7 e e . . . . 
+        . . c e e e e c 7 e 2 2 e e . . 
+        . c e e e e e c 6 e e 2 2 2 e . 
+        . c e e e 2 e c c 2 4 5 4 2 e . 
+        c e e e 2 2 2 2 2 2 4 5 5 2 2 e 
+        c e e 2 2 2 2 2 2 2 2 4 4 2 2 e 
+        c e e 2 2 2 2 2 2 2 2 2 2 2 2 e 
+        c e e 2 2 2 2 2 2 2 2 2 2 2 2 e 
+        c e e 2 2 2 2 2 2 2 2 2 2 2 2 e 
+        c e e 2 2 2 2 2 2 2 2 2 2 4 2 e 
+        . e e e 2 2 2 2 2 2 2 2 2 4 e . 
+        . 2 e e 2 2 2 2 2 2 2 2 4 2 e . 
+        . . 2 e e 2 2 2 2 2 4 4 2 e . . 
+        . . . 2 2 e e 4 4 4 2 e e . . . 
+        . . . . . 2 2 e e e e . . . . . 
+        `,
+    img`
+        ......................bbb.......
+        ....................bb333b......
+        .................bbb333d33b.....
+        ................bb333333d3a.....
+        ..............bb33332eeeedba....
+        ............bbb333323eee2e3a....
+        ..........bbd333333e22222ed3a...
+        .......bbbdd3333333e22222edda...
+        ......bb3d333333333be222eb3d3a..
+        ...bbb3dd33333333333beeeb33d3a..
+        ..b3ddd33333333333333333333dda..
+        bbddd3333333333333333333333dd3a.
+        b33dddddd3333333333333333333d3a.
+        bb3333333ddddd33333333333333dda.
+        bbbbbbb333dd33dddddddddd3333ddba
+        b55553bbbbbb3333dd33333ddd33dd3a
+        b555555555553bbbbbbbb33333dddd3a
+        bd555555555555555dddbaaaaab3d3ba
+        bb55555555555555555dddddddbb33ba
+        b3bb35555555555d5555d55dddddbbba
+        b33333bbb355dd55555d555ddddddbba
+        b5555d333333bbb35dddddd55ddddbba
+        b5d555dd5553333bbbbb3ddddddddb3a
+        b5d555555555555dd3333bbbbbb3db3a
+        bd5d555555d55555dd555ddbbbbbbb3a
+        bbb55dd555555555555555ddddddbb3a
+        ...bbbbdd555ddd5555ddddddddddb3a
+        .......bbbb555555d5ddd5ddddddb3a
+        ...........bbbb55555555555dd533a
+        ...............bbbbddd5d55d5b3ba
+        ...................bbbbddddb3ba.
+        .......................bbbaaaa..
+        `,
+    img`
+        ...........ccccc66666...........
+        ........ccc4444444444666........
+        ......cc444444444bb4444466......
+        .....cb4444bb4444b5b444444b.....
+        ....eb4444b5b44444b44444444b....
+        ...ebb44444b4444444444b444446...
+        ..eb6bb444444444bb444b5b444446..
+        ..e6bb5b44444444b5b444b44bb44e..
+        .e66b4b4444444444b4444444b5b44e.
+        .e6bb444444444444444444444bb44e.
+        eb66b44444bb444444444444444444be
+        eb66bb444b5b44444444bb44444444be
+        fb666b444bb444444444b5b4444444bf
+        fcb666b44444444444444bb444444bcf
+        .fbb6666b44444444444444444444bf.
+        .efbb66666bb4444444444444444bfe.
+        .86fcbb66666bbb44444444444bcc688
+        8772effcbbbbbbbbbbbbbbbbcfc22778
+        87722222cccccccccccccccc22226678
+        f866622222222222222222222276686f
+        fef866677766667777776667777fffef
+        fbff877768f86777777666776fffffbf
+        fbeffeefffeff7766688effeeeefeb6f
+        f6bfffeffeeeeeeeeeeeeefeeeeebb6e
+        f66ddfffffeeeffeffeeeeeffeedb46e
+        .c66ddd4effffffeeeeeffff4ddb46e.
+        .fc6b4dddddddddddddddddddb444ee.
+        ..ff6bb444444444444444444444ee..
+        ....ffbbbb4444444444444444ee....
+        ......ffebbbbbb44444444eee......
+        .........fffffffcccccee.........
+        ................................
+        `,
+    img`
+        ............3333bb..bb33333.....
+        ........3bb31111d3b311d111d33...
+        .......3bdd11111dbd11d11111113..
+        .......bdddd1111bd11d111dd11113.
+        ......3d111dd111b11d111dd33d11d3
+        ......3d11111dd1d11d111d11d33113
+        ....bb3d111111dd13dd111d1dd3b31b
+        ...b3d3dd111111dd13dd11d1dddbbdb
+        ...3ddd31d111111dd133dddddddb.b.
+        ..311111d1ddd1111dd11dddddd33...
+        ..3111111d111dd111dd1111dd3313..
+        ..bddd1111ddd11dd111d111111113..
+        ..311ddd111dddd11dd11ddd1111ddb.
+        ..31111dd111dddd11dd111dddddddb.
+        ...bd1111d1113ddd11dd1111111d3b.
+        ...4dd1111d1113ddd11ddd111d333b.
+        ..4dbdddd11d11133ddddddddddddb..
+        .4ddbddddd11d111d33ddddddddd3b..
+        .4dddb11ddd11dd111d333dddd3bb...
+        .4dd55b111d11dd11111d3333bbb....
+        .445555b111d11dddd111111ddb.....
+        .4455555bd1d311ddddddddddd3.....
+        .45455555bb1d3111ddddddd113.....
+        .4554555555b333d1111111113......
+        455554555555bbb33d11111d33......
+        4d555545555555dbbb3d11d33.......
+        4dd5555455555ddddd43333.........
+        45dd555544ddddddd4..............
+        .45dd5555d44dddd4...............
+        ..45dd55dddd4444................
+        ...45dd55444....................
+        ....44444.......................
+        `,
+    img`
+        . . . . . . . . . . . 6 6 6 6 6 
+        . . . . . . . . . 6 6 7 7 7 7 8 
+        . . . . . . 8 8 8 7 7 8 8 6 8 8 
+        . . e e e e c 6 6 8 8 . 8 7 8 . 
+        . e 2 5 4 2 e c 8 . . . 6 7 8 . 
+        e 2 4 2 2 2 2 2 c . . . 6 7 8 . 
+        e 2 2 2 2 2 2 2 c . . . 8 6 8 . 
+        e 2 e e 2 2 2 2 e e e e c 6 8 . 
+        c 2 e e 2 2 2 2 e 2 5 4 2 c 8 . 
+        . c 2 e e e 2 e 2 4 2 2 2 2 c . 
+        . . c 2 2 2 e e 2 2 2 2 2 2 2 e 
+        . . . e c c e c 2 2 2 2 2 2 2 e 
+        . . . . . . . c 2 e e 2 2 e 2 c 
+        . . . . . . . c e e e e e e 2 c 
+        . . . . . . . . c e 2 2 2 2 c . 
+        . . . . . . . . . c c c c c . . 
+        `
+    ]
+}
+function eatEnemy (mySprite: Sprite, mySprite2: Sprite) {
+    effects.hearts.startScreenEffect(500)
+    scene.cameraShake(4, 500)
+    mySprite.sayText("Tasty!!", 500, true)
+    mySprite2.destroy()
+    info.changeScoreBy(1)
+    createFoodEnemy()
+    mySprite.startEffect(effects.smiles, 500)
+}
 function createPlayer () {
     mySprite = sprites.create(img`
         5 5 5 5 5 5 3 3 3 3 3 3 5 5 5 5 
@@ -51,8 +243,21 @@ function createPlayer () {
         `, SpriteKind.Player)
     controller.moveSprite(mySprite, 100, 100)
 }
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    if (controller.A.isPressed()) {
+        eatEnemy(sprite, otherSprite)
+    }
+    timer.background(function () {
+        timer.after(500, function () {
+            if (sprite.overlapsWith(otherSprite)) {
+                game.over(false)
+            }
+        })
+    })
+})
 let mySprite: Sprite = null
-let myEnemy: Sprite = null
+let foodTypes: Image[] = []
+let mySprite2: Sprite = null
 scene.setBackgroundImage(img`
     dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd
     dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd
@@ -175,6 +380,9 @@ scene.setBackgroundImage(img`
     ddddddd55555555555555555555ddddd55555555555555555555555555555555555555ddddddddddddddddddddddddddddde3355dddddddddddddd5d55b3bdddd444bbdddb4ddd4dd3ee3ddddddddddd
     ddddddddd55555555555555555555ddddddddddddddddddddd555555ddddddddddddddddddddddddddddddddddddddddddde33d55d5555dd555d555333b3bddddddd4e33dbbdd4444eeddddddddddddd
     `)
+initializeGame()
 createPlayer()
-createFoodEnemy()
-createFoodEnemy()
+for (let index = 0; index < 4; index++) {
+    createFoodEnemy()
+}
+game.showLongText("move to food and press A to eat!", DialogLayout.Full)
